5d490a9d7abc93934e1a5cf5e9d6366833886117
Badal <bkbadal7779810465@gmail.com>
2024-06-15 21:53:16 +0530

    health che ckup controller complete

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(cookiesParser());
 
 //routes import
 import userRouter from "./routes/user.routes.js";
-// import healthcheckRouter from "./routes/healthCheck.routes.js"
+import healthcheckRouter from "./routes/healthCheck.routes.js"
 // import tweetRouter from "./routes/tweet.routes.js"
 // import subscriptionRouter from "./routes/subscription.routes.js"
 import videoRouter from "./routes/video.routes.js"
@@ -38,7 +38,7 @@ import playlistRouter from "./routes/playlist.routes.js"
 // import dashboardRouter from "./routes/dashboard.routes.js"
 
 // routes declaration
-// app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
 app.use("/api/v1/users", userRouter);
 // app.use("/api/v1/tweets", tweetRouter)
 // app.use("/api/v1/subscriptions", subscriptionRouter)
--- src/controllers/healthCheck.controller.js
+++ src/controllers/healthCheck.controller.js
@@ -1,11 +1,24 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponce.js";
+import asyncHandler from "../utils/asyncHandler.js"
+import { isValidObjectId } from "mongoose"
 
 
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
-    return res.status(200).json(new Api)
+    // const userId=req.params;
+    // if(!userId){
+    //     throw new ApiError(400, "Bad request")
+    // }
+    // if(!isValidObjectId(userId)){
+    //     throw new ApiError(400, "Bad request")
+    // }
+
+    return res.status(200).json(new ApiResponse(
+        200,
+        {},
+        "Everything is okk"
+    ))
 })
 
 export {
